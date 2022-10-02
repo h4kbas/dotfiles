6de06da274b847143255e9587cbcabb5fba2e69e
Hüseyin Akbaş <h4kbas@gmail.com>
2022-10-02 14:12:27 +0300

    Fonts added

--- mac/setup.sh
+++ mac/setup.sh
@@ -125,6 +125,10 @@ defaults write com.apple.finder DisableAllAnimations -bool true
 
 killall Finder
 
+### Fonts ###
+
+cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+
 ### Applications ###
 
 # Install Firefox
